123131de6005a857317a6910cfe19aa82a51fc5e
fiopwk <flopwiki@gmail.com>
2022-07-12 12:44:21 -0400

    using string formatting and string functions

--- week_1/ProblemSet0/hello.py
+++ week_1/ProblemSet0/hello.py
@@ -1,1 +1,10 @@
-print("hello world")
+def getUserName() -> str:
+    # ask user for username
+    username = input("What's your name? ").strip()
+    codingCourseName = "intro to programming with python!".title()
+
+    # say hello to username
+    print("hello, {}".format(username))
+    print(f'Welcome to "{codingCourseName}", ' + username)
+
+getUserName()
