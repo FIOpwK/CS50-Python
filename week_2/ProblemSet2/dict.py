cc5dfdd2be6a091579af6769968e22b6880295c5
fiopwk <flopwiki@gmail.com>
2022-07-13 22:34:24 -0400

    week 3: working with exceptions

---
+++ week_2/ProblemSet2/dict.py
@@ -0,0 +1,10 @@
+name = input("What's the player name? ")
+house = input("What's the player house? ")
+patronus = input("What's the player patronus? ")
+
+
+students = [
+    {"name": name, "house": house, "patronus": patronus}
+]
+
+print(students)
